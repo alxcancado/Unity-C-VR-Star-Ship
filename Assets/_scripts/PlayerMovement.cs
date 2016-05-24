784cb1a5354dfc6f7dae30ad9cb324cd73f7dcc7
alxcancado <alxcancado@gmail.com>
2016-05-24 15:36:58 +1200

    Player Setup

---
+++ Assets/_scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float minX, minY, maxX, maxY;
+
+	Rigidbody playerRigidbody;
+
+	void Awake(){
+
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+
+		playerRigidbody.position = new Vector3(
+			Mathf.Clamp(playerRigidbody.position.x, minX, maxX),
+			Mathf.Clamp(playerRigidbody.position.y, minY,maxY),
+			0.0f
+		);
+
+		playerRigidbody.rotation = Quaternion.identity; //Quaternion.Euler(0.0f, 0.0f, 0.0f)
+	}
+}
